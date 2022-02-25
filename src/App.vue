d3fa2ae1a353d34feb5ddbbbf2a04dafc63725fc
Erin Jackson <erinebjackson@gmail.com>
2022-02-24 18:51:13 -0800

    updates movie frontend with bootstrap elements

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,42 @@
 <script>
 export default {
-  methods: {
-    isLoggedIn: function() {
-      return localStorage.getItem('jwt');
-    }
-  }
-
+  data: function () {
+    return {
+      isLoggedIn: !!localStorage.jwt,
+    };
+  },
+  watch: {
+    $route: function () {
+      this.isLoggedIn = !!localStorage.jwt;
+    },
+  },
 };
 </script>
 
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link>|
-    <router-link to="/movies">Movies</router-link> |
-    <span v-if="isLoggedIn()">
-    <router-link to="/logout">Logout</router-link> |
-    <router-link  to="/movies/new">New</router-link> 
-    </span>
-    <span v-else>
-      <router-link to="/login">Login</router-link> |
-      <router-link to="/signup">Signup</router-link> |
-    </span>
+  <nav class="navbar navbar-light bg-light">
+    <div class="container">
+      <a class="navbar-brand" href="#">
+        <img
+          src="https://images.creativemarket.com/0.1.0/ps/7414066/1820/1214/m1/fpnw/wm1/logo-design-for-movie-production-company-01-.jpg?1575502358&s=c37b3e6a8863b415070b669f6c8a457c"
+          alt=""
+          width="60"
+          height="44"
+        />
+      </a>
+      <nav class="nav">
+        <router-link class="nav-link active" aria-current="page" to="/">Home</router-link>
+        <router-link class="nav-link active" aria-current="page" to="/movies">Movies</router-link>
 
-  </div>
+        <router-link v-if="isLoggedIn" class="nav-link" to="/logout">Logout</router-link>
+
+        <router-link v-if="isLoggedIn" class="nav-link" to="/movies/new">New</router-link>
+        <router-link v-if="!isLoggedIn" class="nav-link" to="/login">Login</router-link>
+        <router-link v-if="!isLoggedIn" class="nav-link" to="/signup">Signup</router-link>
+      </nav>
+    </div>
+  </nav>
+
   <router-view />
 </template>
 
@@ -46,27 +60,27 @@ export default {
   font-weight: bold;
   color: #fff;
   padding: 8px;
-  
 }
 
 #nav a.router-link-exact-active {
   color: #42b983;
 }
-button, input.submit {
+button,
+input.submit {
   color: #147060;
-    background-color: #fff;
-    border: 2px solid #147060;
-    border-radius: 5px;
-    padding: 5px 10px;
-    margin: 10px 10px;
+  background-color: #fff;
+  border: 2px solid #147060;
+  border-radius: 5px;
+  padding: 5px 10px;
+  margin: 10px 10px;
 }
 .main-content {
   margin-top: 50px;
 }
 a {
-  color:#1b9480;
+  color: #1b9480;
 }
 a:visited {
-  color: #0a3f36
+  color: #0a3f36;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,104 +1,162 @@
-<script>import axios from "axios";
+<script>
+import axios from "axios";
 
-  export default {
-    data: function () {
-      return {
-        message: "",
-        movies: [],
-        createParams: {},
-        currentMovie: {},
-        updateParams: {}
-
-      };
-    },
-    created: function () {
-      this.indexMovies();
-    },
-    methods: {
-      indexMovies: function() {
-        axios.get("/movies").then((response) => { console.log(response.data);
-        this.movies = response.data 
+export default {
+  data: function () {
+    return {
+      message: "",
+      movies: [],
+      createParams: {},
+      currentMovie: {},
+      updateParams: {},
+    };
+  },
+  created: function () {
+    this.indexMovies();
+  },
+  methods: {
+    indexMovies: function () {
+      axios.get("/movies").then((response) => {
+        console.log(response.data);
+        this.movies = response.data;
       });
-      },
-      createMovies: function() {
-        axios.post("/movies", this.createParams).then((response) => { console.log("success", response.data);
+    },
+    createMovies: function () {
+      axios.post("/movies", this.createParams).then((response) => {
+        console.log("success", response.data);
         this.movies.push(response.data);
         this.createParams = {};
-      })
+      });
     },
-      showMovie: function(movie) {
-        console.log(movie);
-        this.currentMovie = movie;
-        // this.updateParams = movie;
-        document.querySelector("#movieDetails").showModal();
-      },
+    showMovie: function (movie) {
+      console.log(movie);
+      this.currentMovie = movie;
+      // this.updateParams = movie;
+      document.querySelector("#movieDetails").showModal();
+    },
 
-     updateMovie: function (movie) {
-       
-       axios.patch(`/movies/${movie.id}`, movie).then((response) => {
-         console.log("success!", response.data);
-       })
-       .catch((error) => {
-         console.log(error.response.data.errors);
-       })
-     },
-    destroyMovie: function(movie) {
+    updateMovie: function (movie) {
+      axios
+        .patch(`/movies/${movie.id}`, movie)
+        .then((response) => {
+          console.log("success!", response.data);
+        })
+        .catch((error) => {
+          console.log(error.response.data.errors);
+        });
+    },
+    destroyMovie: function (movie) {
       axios.delete(`/movies/${movie.id}`).then((response) => {
         console.log("success", response.data);
         var i = this.movie.indexOf(movie);
         this.movie.splice(i, 1);
       });
-    }
     },
-  };
+  },
+};
 </script>
 
 <template>
-
   <div class="home">
     <div class="hero">
-    <h1>Welcome to the movie app!</h1>
+      <h1>Welcome to the movie app!</h1>
     </div>
-    <div class="movie-list">
-    <h3>Check out the movie recomendations </h3>
-<div v-for=" movie in movies" v-bind:key="movie.id">
-<ul>
-<li> {{movie.title}}<br>
-<button v-on:click="showMovie(movie)">More Info</button></li></ul>
-</div>
+    <!-- <div class="movie-list">
+      <h3>Check out the movie recomendations</h3>
+      <div v-for="movie in movies" v-bind:key="movie.id">
+        <h4>{{ movie.title }}</h4>
+        <br />
+        {{ movie.plot }}
+        <br />
+        <button v-on:click="showMovie(movie)">More Info</button>
+      </div>
 
-<dialog id="movieDetails">
-  <form method="dialog" class="popup">
-    <h3>Movie Info</h3>
-  <p>{{currentMovie.title}}</p>
-  <p>{{currentMovie.year}}</p>
-  <p>{{currentMovie.plot}}</p>
-  <h3>Enter Movie Updates</h3>
-  
-  <input type="text"  v-model="updateParams.title" placeholder="title"><br>
-  <input type="text"  v-model="updateParams.year" placeholder="year"><br>
-  <input type="text" placeholder="plot" v-model="updateParams.plot"><br>
-  <input type="text" placeholder="director" v-model="updateParams.director"><br>
-  <input type="text" placeholder="english" v-model="updateParams.english"><br>
-  <input type="text" placeholder="runtime" v-model="updateParams.runtime"><br>
-  <button v-on:click="updateMovie(currentMovie)">Update</button>
-  <button v-on:click="destroyMovie(currentMovie)">Delete</button> <button>Close</button>
-  </form>
+      <dialog id="movieDetails">
+        <form method="dialog" class="popup">
+          <h3>Movie Info</h3>
+          <p>{{ currentMovie.title }}</p>
+          <p>{{ currentMovie.year }}</p>
+          <p>{{ currentMovie.plot }}</p>
+          <h3>Enter Movie Updates</h3>
 
-</dialog>
-</div>
-<div class="add-movie">
-<h2>Add a Movie:</h2>
-<form>
-<input type="text" v-model="createParams.title" placeholder="title"><br>
-<input type="text" v-model="createParams.year" placeholder="year"><br>
-<input type="text" v-model="createParams.plot" placeholder="plot"><br>
-<input type="text" v-model="createParams.director" placeholder="director"><br>
-<input type="text" v-model="createParams.english" placeholder="english"><br>
-<input type="text" v-model="createParams.runtime" placeholder="runtime"><br>
-<button v-on:click="createMovies()">Add Movie</button>
-</form>
-</div>
+          <input type="text" v-model="updateParams.title" placeholder="title" />
+          <br />
+          <input type="text" v-model="updateParams.year" placeholder="year" />
+          <br />
+          <input type="text" placeholder="plot" v-model="updateParams.plot" />
+          <br />
+          <input type="text" placeholder="director" v-model="updateParams.director" />
+          <br />
+          <input type="text" placeholder="english" v-model="updateParams.english" />
+          <br />
+          <input type="text" placeholder="runtime" v-model="updateParams.runtime" />
+          <br />
+          <button v-on:click="updateMovie(currentMovie)">Update</button>
+          <button v-on:click="destroyMovie(currentMovie)">Delete</button>
+          <button>Close</button>
+        </form>
+      </dialog>
+    </div> -->
+    <div class="container mt-5">
+      <div class="row row-cols-1 row-cols-md-2 g-4">
+        <div class="col" v-for="movie in movies" v-bind:key="movie.id">
+          <div class="card">
+            <img v-bind:src="movie.image_url" class="card-img-top" alt="..." />
+            <div class="card-body">
+              <h5 class="card-title">{{ movie.title }}</h5>
+              <p class="card-text">
+                {{ movie.plot }}
+              </p>
+              <button v-on:click="showMovie(movie)" type="button" class="btn btn-secondary btn-sm">More Info</button>
+              <!-- <button v-on:click="showMovie(movie)">More Info</button> -->
+            </div>
+            <dialog id="movieDetails">
+              <form method="dialog" class="popup">
+                <h3>Movie Info</h3>
+                <p>{{ currentMovie.title }}</p>
+                <p>{{ currentMovie.year }}</p>
+                <p>{{ currentMovie.plot }}</p>
+                <h3>Enter Movie Updates</h3>
+
+                <input type="text" v-model="updateParams.title" placeholder="title" />
+                <br />
+                <input type="text" v-model="updateParams.year" placeholder="year" />
+                <br />
+                <input type="text" placeholder="plot" v-model="updateParams.plot" />
+                <br />
+                <input type="text" placeholder="director" v-model="updateParams.director" />
+                <br />
+                <input type="text" placeholder="english" v-model="updateParams.english" />
+                <br />
+                <input type="text" placeholder="runtime" v-model="updateParams.runtime" />
+                <br />
+                <button v-on:click="updateMovie(currentMovie)">Update</button>
+                <button v-on:click="destroyMovie(currentMovie)">Delete</button>
+                <button>Close</button>
+              </form>
+            </dialog>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="add-movie">
+      <h2>Add a Movie:</h2>
+      <form>
+        <input type="text" v-model="createParams.title" placeholder="title" />
+        <br />
+        <input type="text" v-model="createParams.year" placeholder="year" />
+        <br />
+        <input type="text" v-model="createParams.plot" placeholder="plot" />
+        <br />
+        <input type="text" v-model="createParams.director" placeholder="director" />
+        <br />
+        <input type="text" v-model="createParams.english" placeholder="english" />
+        <br />
+        <input type="text" v-model="createParams.runtime" placeholder="runtime" />
+        <br />
+        <button v-on:click="createMovies()">Add Movie</button>
+      </form>
+    </div>
   </div>
 </template>
 
@@ -110,15 +168,15 @@
 .hero {
   background-image: url("https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/best-movies-1614634680.jpg");
   background-size: cover;
-    height: 375px;
-    padding-top: 1px;
+  height: 375px;
+  padding-top: 1px;
 }
 .hero h1 {
   margin-top: 150px;
   color: rgb(100, 100, 101);
-    font-size: 3.3em;
-    text-transform: capitalize;
-    background-color: rgba(255, 255, 255, 0.78);;
+  font-size: 3.3em;
+  text-transform: capitalize;
+  background-color: rgba(255, 255, 255, 0.78);
 }
 h3 {
   text-transform: capitalize;
@@ -131,12 +189,12 @@ ul li {
   list-style-type: none;
   text-align: center;
   width: 33%;
-    float: left;
-    text-transform: capitalize;
-    padding-top: 10px;
+  float: left;
+  text-transform: capitalize;
+  padding-top: 10px;
 }
 .movie-list {
-  margin-bottom: 30px;;
+  margin-bottom: 30px;
 }
 .add-movie {
   width: 100%;
